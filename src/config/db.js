dfb9ae8b7b59dfc0faf07abb3462a11e59d87184
qwerty12-ai <sabeehmohd79@gmail.com>
2026-06-24 17:29:30 +0530

    add SSL support for hosted MySQL deployment

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,12 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: useSSL 
+    ? {
+        rejectUnauthorized: false
+    }: undefined
 })
 
 module.exports = pool
